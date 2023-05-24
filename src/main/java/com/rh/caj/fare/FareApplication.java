373af356e813621f1cdb7ab45b6fe06bc03b53ed
zxc81906119 <35023416+zxc81906119@users.noreply.github.com>
2023-05-24 17:07:13 +0800

    Update FareApplication.java

--- src/main/java/com/rh/caj/fare/FareApplication.java
+++ src/main/java/com/rh/caj/fare/FareApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FareApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(FareApplication.class, args);
 	}
 
